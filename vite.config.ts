c993e04d290fcc9552c63d2a5fa305e6b5c0d7db
Yoga S Devanada <yogasdevanada@gmail.com>
2024-07-22 20:31:55 +0700

    feat: implement pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,52 @@
 /// <reference types="vitest" />
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
 import million from "million/compiler";
 import { defineConfig } from "vite";
 
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: "prompt",
+  includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: "Weather Ups",
+    short_name: "Weathe Ups",
+    description: "An app that can show weather forecast for your city.",
+    icons: [
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon",
+      },
+      {
+        src: "/maskable_icon.png",
+        sizes: "200x200",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+    theme_color: "#171717",
+    background_color: "#e8ebf2",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [million.vite({ auto: true }), react()],
+  plugins: [million.vite({ auto: true }), react(), VitePWA(manifestForPlugin)],
   server: {
     host: true,
   },
